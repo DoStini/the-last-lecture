72bf5024f181bbf3e4cb8678d6645b1950cd7406
Nuno Alves <nyansword@gmail.com>
2023-02-28 22:13:42 +0000

    add: animation of damage popup

--- Assets/Scripts/GameManager/DamageObserver.cs
+++ Assets/Scripts/GameManager/DamageObserver.cs
@@ -3,11 +3,31 @@ using UnityEngine;
 
 public class DamageObserver : MonoBehaviour
 {
-    public List<float> colorThresholds;
+    /**
+     * Sorted list of color thresholds. If damage passes a certain threshold, a different color is used.
+     */
+    public List<int> colorThresholds;
+    /**
+     * Number of colors is the size of color thresholds + 1. The first color is the default color, the second color is
+     * used when damage passes the first threshold and so on.
+     */
     public List<Color> colors;
+
+    private Color getColor(int damage)
+    {
+        int index = colorThresholds.BinarySearch(damage);
+        if (index < 0)
+        {
+            index = ~index;
+        }
+
+        return colors[index];
+    }
 
     public void HandleDamagePopup(Character character, int damageTaken)
     {
+        Debug.Log("------");
         Debug.Log(character + " took " + damageTaken);
+        Color color = getColor(damageTaken);
     }
 }
