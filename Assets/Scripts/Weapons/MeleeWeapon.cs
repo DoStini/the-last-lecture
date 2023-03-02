1d0301442dbb2a0c50a69269771de575b1f8d3bf
Nuno Alves <nyansword@gmail.com>
2023-03-02 13:58:54 +0000

    add: weapon animation callback and trigger, add dealing damage

--- Assets/Scripts/Weapons/MeleeWeapon.cs
+++ Assets/Scripts/Weapons/MeleeWeapon.cs
@@ -1,16 +1,16 @@
-using System;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 public class MeleeWeapon : Weapon
 {
     public Transform playerCenter;
     public Vector3 boxSize;
+    public Animator animator;
     public float knockbackFactor;
-    private bool m_start = false;
+    private bool m_start;
 
     private Collider[] _colliders;
-    private Vector3 boxCenter;
+    private Vector3 _boxCenter;
+    private static readonly int Attack1 = Animator.StringToHash("Attack");
 
     private void Start()
     {
@@ -20,16 +20,25 @@ public class MeleeWeapon : Weapon
 
     protected override bool _Attack(Vector3 pointerLocation)
     {
-        boxCenter = playerCenter.position + playerCenter.rotation * Vector3.forward * boxSize.z;
-
-        Debug.Log(playerCenter.rotation.eulerAngles);
-        Debug.Log(Physics.OverlapBoxNonAlloc(boxCenter, boxSize, _colliders, playerCenter.rotation, mask));
+        animator.SetTrigger(Attack1);
         return true;
     }
 
     private void DealDamage()
     {
+        var rotation = playerCenter.rotation;
         
+        _boxCenter = playerCenter.position + rotation * Vector3.forward * boxSize.z;
+        int numColliders = Physics.OverlapBoxNonAlloc(_boxCenter, boxSize, _colliders, rotation, mask);
+
+        for (int i = 0; i < numColliders; i++)
+        {
+            Collider hitCollider = _colliders[i];
+            if (!hitCollider.gameObject.CompareTag("Enemy") && !hitCollider.gameObject.CompareTag("Player")) return;
+
+            Character character = hitCollider.gameObject.GetComponent<Character>();
+            character.RemoveHealth(damageStrategy.CalculateDamage());
+        }
     }
 
     private void OnDrawGizmos()
@@ -37,7 +46,7 @@ public class MeleeWeapon : Weapon
 // cache previous Gizmos settings
         if (m_start)
         {
-            boxCenter = playerCenter.position + playerCenter.rotation * Vector3.forward * boxSize.z;
+            _boxCenter = playerCenter.position + playerCenter.rotation * Vector3.forward * boxSize.z;
 
             Color prevColor = Gizmos.color;
             Matrix4x4 prevMatrix = Gizmos.matrix;
@@ -46,7 +55,7 @@ public class MeleeWeapon : Weapon
             Gizmos.matrix = playerCenter.localToWorldMatrix;
 
             // convert from world position to local position 
-            Vector3 boxPosition = playerCenter.InverseTransformPoint(boxCenter);
+            Vector3 boxPosition = playerCenter.InverseTransformPoint(_boxCenter);
 
             Gizmos.DrawWireCube(boxPosition, boxSize * 2f);
 
