a45eef9c146739f464d4362a6128e4463c9ae139
Nuno Alves <nyansword@gmail.com>
2023-03-05 00:54:38 +0000

    add: strafing, backwards, and running animations

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,7 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] public float speedFactor = 6f;
     [SerializeField] public float gravityFactor = 9.8f;
     [SerializeField] public Player player;
+    [SerializeField] private Animator animator;
 
     private PlayerInputActions _playerActions;
     private InputAction _moveAction;
@@ -15,15 +16,18 @@ public class PlayerMovement : MonoBehaviour
     private float _vSpeed;
     private float _lookAngle;
     private Vector2 _actionDirection = Vector2.zero;
-    private Vector3 _pointerLocation = Vector2.zero;
+    private Vector3 _pointerLocation = Vector3.zero;
+    private Vector2 _lookDirection = Vector2.zero;
     private bool _shootHeld;
     private int _holdTime;
+    private static readonly int Speed = Animator.StringToHash("Speed");
+    private static readonly int Angle = Animator.StringToHash("Angle");
 
     public void UpdateLookAngle(Vector3 pointerLocation)
     {
         var position = transform.position;
-        var lookDirection = new Vector2(pointerLocation.x - position.x, pointerLocation.z - position.z);
-        _lookAngle = Mathf.Atan2(lookDirection.x, lookDirection.y) * Mathf.Rad2Deg;
+        _lookDirection = new Vector2(pointerLocation.x - position.x, pointerLocation.z - position.z);
+        _lookAngle = Mathf.Atan2(_lookDirection.x, _lookDirection.y) * Mathf.Rad2Deg;
         _pointerLocation = pointerLocation;
     }
 
@@ -77,6 +81,18 @@ public class PlayerMovement : MonoBehaviour
         else
         {
             _holdTime = 0;
+        }
+
+        if (_actionDirection.magnitude > 0)
+        {
+            animator.SetFloat(Speed, speedFactor/6);
+            var angle = Vector2.SignedAngle(_lookDirection, _actionDirection);
+
+            animator.SetFloat(Angle, angle, 0.1f, Time.deltaTime);
+        }
+        else
+        {
+            animator.SetFloat(Speed, 0);
         }
     }
 
